28c2a4e83220f5bb16c20d0b1a2a8cd02bfe8306
Luigigi94 <el.luige94@gmail.com>
2026-03-23 23:09:25 -0600

    WIP (ReserveView): creating ReserveView

---
+++ src/views/ReserveView.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import {onMounted, onUnmounted, watch} from "vue";
+import {useReservesStore} from "@/store/ReservesStore";
+import {useI18n} from "vue-i18n";
+
+import Dialog from "primevue/dialog";
+
+const reserveStore = useReservesStore();
+const { t } = useI18n();
+
+onMounted(() => {
+  reserveStore.getAllReserves()
+})
+
+onUnmounted(() => {
+  reserveStore.clearState()
+})
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
